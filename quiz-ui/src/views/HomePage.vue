5b44cea28a93245ec1df4ac074e32ba7fa9d6794
audric <audric.daviau.leymet@gmail.com>
2023-05-11 11:28:57 +0200

    homepage update

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores: [], // Variable tableau des scores
+    };
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+    try {
+      // Appel au service pour récupérer les scores
+      const scores = await quizApiService.getScores();
+      this.registeredScores = scores; // Stockage des scores dans registeredScores
+    } catch (error) {
+      console.error("Erreur lors de la récupération des scores:", error);
+    }
+  },
+};
+</script>
