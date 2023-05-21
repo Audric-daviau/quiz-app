6700576b4e191ce69e47c907c80dfa56511a458d
sageekar <ragavi.sageekaran@edu.esiee.fr>
2023-05-21 16:07:30 +0200

    première page admin

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue'
+import Admin from '../views/Admin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +11,9 @@ const router = createRouter({
       component: HomePage,
     },
     {
-      path: "/",
+      path: "/admin",
       name: "Admin",
-      component: HomePage,
+      component: Admin,
     },
     // ... autres routes
     {
--- quiz-ui/src/views/Admin.vue
+++ quiz-ui/src/views/Admin.vue
@@ -27,6 +27,7 @@
   
   <script>
   export default {
+    name: "Admin",
     data() {
       return {
         password: '',
