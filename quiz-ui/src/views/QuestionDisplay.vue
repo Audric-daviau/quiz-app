b072f01cb80c4b8a7462701cf802649d9bf3d38f
audric <audric.daviau.leymet@gmail.com>
2023-05-23 17:58:16 +0200

    affichage bizzare

--- quiz-ui/src/views/QuestionDisplay.vue
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -1,40 +1,35 @@
 <template>
-  <div v-if="question && question.title && question.description">
-    <h2>{{ question.title }}</h2>
-    <p>{{ question.description }}</p>
-    <img v-if="question.image" :src="question.image" />
-    <ul v-if="question.options">
-      <li v-for="option in question.options" :key="option.id" @click="selectOption(option)">
-        {{ option.text }}
-      </li>
-    </ul>
-    <a @click="selectOption(2)">La réponse D</a>
-  </div>
-  <div v-else>
-    Loading question...
-  </div>
+    <div>
+        <h2>{{ question.title }}</h2>
+        <p>{{ question.description }}</p>
+        <img v-if="question.image" :src="question.image" />
+        <ul>
+            <li v-for="option in question.options" :key="option.id" @click="selectOption(option)">
+                {{ option.text }}
+            </li>
+        </ul>
+        <a @click="selectOption(2)">La réponse D</a>
+    </div>
 </template>
-
-
   
-  <script>
-  export default {
+<script>
+export default {
     name: 'QuestionDisplay',
     props: {
-      question: {
-        type: Object,
-      }
+        question: {
+            type: Object,
+        }
     },
     emits: ['answer-selected'], // Déclaration de l'événement émis
     methods: {
-      selectOption(option) {
-        this.$emit('answer-selected', option);
-      }
+        selectOption(option) {
+            this.$emit('answer-selected', option);
+        }
     }
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  /* Styles spécifiques au composant QuestionDisplay */
-  </style>
+<style scoped>
+/* Styles spécifiques au composant QuestionDisplay */
+</style>
   
